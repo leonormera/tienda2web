331cdbe4c1f00cc85fe807659d5a7d7e909619f4
Efren Villamagua <efren.villamagua@gmail.com>
2015-12-08 20:14:04 -0500

    c

---
+++ tienda2web/src/tienda2web/app/resources/DDL_01.sql
@@ -0,0 +1,8 @@
+SELECT * from bairesdev_db.categoria;
+	INSERT INTO bairesdev_db.categoria
+	(OID,CODIGO,NOMBRE)VALUES('1','C01','Electrónica');
+	INSERT INTO bairesdev_db.categoria
+	(OID,CODIGO,NOMBRE)VALUES('2','C02','Ferretería');
+	INSERT INTO bairesdev_db.categoria
+	(OID,CODIGO,NOMBRE)VALUES('3','C03','Comestibles');
+	SELECT * from bairesdev_db.producto;
